c4bf2aa6ae290de237b8d27b603de3ab55a67390
daoyi2026 <daoyi2026@users.noreply.github.com>
2026-09-20 16:16:22 +0800

    fix: refine overview banner typography

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "kinetic-life-os-v50-shell";
+const CACHE_NAME = "kinetic-life-os-v51-shell";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./app-v2.css?v=87",
+  "./app-v2.css?v=88",
   "./app-v2.js?v=87",
   "./assets/icon-smile-180.png",
   "./assets/icon-smile-192.png",
